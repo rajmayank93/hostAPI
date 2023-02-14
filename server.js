f2234719a651dd5743aecbd1b269a1b3cd0e62dd
Mayank Gupta <rajmayank93@gmail.com>
2023-02-14 21:07:20 +0530

    updated

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ app.get('/', function (req, res) {
    });
 })
 
-var server = app.listen(8081, function () {
+
+var PORT = process.env.port || 8081;
+var server = app.listen(PORT, function () {
    var host = server.address().address
    var port = server.address().port
    console.log("Example app listening at http://%s:%s", host, port)
