0ac61129c950200097d55db6a9334e02d446b1eb
Mayank Gupta <rajmayank93@gmail.com>
2023-02-14 21:38:04 +0530

    Updated

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', function (req, res) {
 
 const port = process.env.PORT || 8081;
 var server = app.listen(port, function () {
-   var host = server.address().address
-   var port = server.address().port
-   console.log("Example app listening at http://%s:%s", host, port)
+   // var host = server.address().address
+   // var port = server.address().port
+   console.log(`Example app listening at ${port}`)
 })
